4a7fcb959c55ce614c4215f1e7d81c9a3926effe
temkooo97 <temkooo97@gmail.com>
2021-05-18 20:35:12 +0400

    Add Third task

--- src/com/artem/idea/Task_3.java
+++ src/com/artem/idea/Task_3.java
@@ -5,6 +5,7 @@ public class Task_3 {
         double [] myList = {1.9,2.9,3.4,3.5,3.6};
         double temp = myList[0];
         myList[0]=myList[4];
+
         myList[3]= temp;
         double result = myList[0]+myList[2];
         System.out.println(result);
