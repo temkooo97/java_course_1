9a500e4e2cf950d2ff26b5288c6e826ba86f8c18
temkooo97 <temkooo97@gmail.com>
2021-05-18 20:45:56 +0400

    Add Task 8 and clear Main class

--- src/com/artem/idea/Main.java
+++ src/com/artem/idea/Main.java
@@ -4,39 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter array length: ");
-        int size = scanner.nextInt();
-        double[] Array;
-        Array = new double[size];
-        System.out.println("Insert array elements:");
-        for (int i = 0; i < size; i++) {
-            Array[i] = scanner.nextInt();
-        }
-        double average = 0;
 
-        for (int i = 0; i < Array.length; i++) {
-            if (Array.length > 0) {
-                double sum = 0;
-                for (int j = 0; j < Array.length; j++) {
-
-                    sum += Array[j];
-                }
-                average = sum / Array.length;
-            }
-        }
-            System.out.println("Среднее арифметическое = " + average);
-
-            for (int i1 = 0; i1 < Array.length; i1++){
-
-                System.out.println("Результат " + (Array[i1] * average) );
-            }
-
-
-
-
-    }
 }
 
 
