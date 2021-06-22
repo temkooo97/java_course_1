9d2c4aa6ff66861357984e210e03c7432bae8688
temkooo97 <temkooo97@gmail.com>
2021-06-22 22:46:06 +0400

    Add Task 11

---
+++ src/com/artem/idea/Task_2.java
@@ -0,0 +1,13 @@
+package com.artem.idea;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        double a;
+        byte b;
+
+        a = 124.33;
+        b = 127;
+
+        System.out.println("Перменная с плавающей точкой, произвольное число = " + a + "\nНаибольшее число в переменной наименьшего диапазона = " + b );
+    }
+}
